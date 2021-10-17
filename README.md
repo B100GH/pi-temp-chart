646eb84a8c110dab6d75a7ab24d9313c9f6bff8e
Benjamin Moisan <benjaminmoisan@outlook.com>
2021-10-16 20:41:44 -0800

    UMM i made it actually work

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pi temp chart
- 
+ this script makes a chart and tells you the cpu temp of your raspberry pi
--- gettemp.py
+++ gettemp.py
@@ -2,21 +2,27 @@ from gpiozero import CPUTemperature
 import time
 
 
-x = int(input('Number of Repeats -- '))
-y = int(input('How often it pulls temp (sec) -- '))
+x = int(input('Number of Repeats ---------------- '))
+y = int(input('How often it pulls temp (sec) ---- '))
 
 
 i = 1
 while i < x:
     
-    time.sleep(y) # Sleep for 60 seconds
+    cpu = CPUTemperature()
+    cpufinal = CPUTemperature()
+
+    time.sleep(y)
     
     cpu = int(cpu.temperature)
-    graph = cpu//5
+    
+    graph = cpu #or //2 or //5
+    graph = round(graph, 1)
+    
     graph = int(cpu)
 
-    print(graph * '-' 'Temp -' + cpu )
-
+    print(graph *'-')
+    print(cpufinal)
 
     i = i + 1
 

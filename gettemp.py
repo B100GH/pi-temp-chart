a919ab5c51d18a50ad14f035383784807eb5928c
B100 <benjaminmoisan@outlook.com>
2021-10-16 16:27:26 -0800

    Update gettemp.py

--- gettemp.py
+++ gettemp.py
@@ -1,7 +1,12 @@
 from gpiozero import CPUTemperature
+import time
 
 i = 1
-while i <=100:
+while i <=60:
+    
+    time.sleep(60) # Sleep for 60 seconds
+    
+    
     cpu = CPUTemperature()
     print(print(cpu.temperature))
     i = i + 1

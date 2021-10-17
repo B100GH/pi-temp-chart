94fd7344536df1cee3b17f5a8c3f8f0ac02f9eb1
B100 <benjaminmoisan@outlook.com>
2021-10-16 16:46:19 -0800

    Update gettemp.py

--- gettemp.py
+++ gettemp.py
@@ -2,22 +2,20 @@ from gpiozero import CPUTemperature
 import time
 
 
-x = int.input('Number of Repeats -- ')
-y = int.input('How often it pulls temp (sec) -- ')
+x = int(input('Number of Repeats -- '))
+y = int(input('How often it pulls temp (sec) -- '))
 
 
 i = 1
-while i <=x:
+while i < x:
     
     time.sleep(y) # Sleep for 60 seconds
     
     
     cpu = CPUTemperature()
-    print(print(cpu.temperature))
+    print(cpu.temperature)
     i = i + 1
 
 print('''
-done
-''')
-
-print(x + )
+The Tracking has completed ''')
+ 
